aa5cd90147c33c8c31cad97285bf9e3fb81c4783
Swas26 <swas2625@gmail.com>
2025-08-26 07:07:31 +1000

    initial setup si w/boilerplate

---
+++ src/Game.js
@@ -0,0 +1,28 @@
+const GameWidth =640;
+const GameHeight = 360;
+
+
+
+let gameScene = new Phaser.Scene("Game");
+
+
+let config = {
+    type: Phaser.AUTO,
+    width: GameWidth,
+    height: GameHeight,
+    scene: gameScene
+};
+
+let game = new Phaser.Game(config);
+
+gameScene.preload = function(){
+    // this.load.image('background', 'assets/background.jpeg');
+};
+
+
+// once preload ends we use creat..
+gameScene.create = function() {
+    // this.add.sprite(0,0,'background').setPosition(GameWidth/2, GameHeight/2).setScale(0.9,0.9);
+};
+
+
